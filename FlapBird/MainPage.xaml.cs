3486941988954ea1b8bcd4c744eee66ddbab9436
Guilherme <gui.igarashi@hotmail.com>
2024-10-15 10:59:03 -0300

    virou mario (culpa do matheus)

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -3,10 +3,75 @@
 public partial class MainPage : ContentPage
 {
 
+	const int gravidade = 10;
+
+	const int maxPulo = 3;
+
+	int tempoPulando = 0;
+
+	bool isJumping = false;	
+
+	const int fps = 300;
+
+	bool dead = true;
+	
+	double LarguraJanela;
+
+	double AlturaJanela;
+
+	int speed = 80;
+
+
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+
+
+	void Inicializar()
+	{
+		dead = false;
+		Mario.TranslationY = 0;
+	}
+
+	async Task Drawn()
+	{
+		while (!dead)
+		{
+			CreateGravity();
+			AndarPipe();
+			await Task.Delay(fps);
+
+		}
+	}
+
+	async void CreateGravity()
+	{
+		Mario.TranslationY += gravidade;
+	}
+
+	void AndarPipe()
+	{
+		CanoCima.TranslationX -= speed;
+		CanoBaixo.TranslationX -= speed;
+
+		if (CanoCima.TranslationX < -LarguraJanela)
+		{
+			CanoCima.TranslationX = 0;
+			CanoBaixo.TranslationX = 0;
+		}
+
+	}
+
+		void OnGameOverClicked(object s, TappedEventArgs a)
+
+		{
+			FrameGameOver.IsVisible=false;
+			Inicializar();
+			Drawn();
+		}
 
 
 }
