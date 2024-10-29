8dbb05dc9e72364f5aecc5d0a61962f922c0f7fc
Guilherme <gui.igarashi@hotmail.com>
2024-10-29 10:29:15 -0300

    fui levado ao mau caminho

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -2,204 +2,166 @@
 
 public partial class MainPage : ContentPage
 {
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	const int gravidade = 20;
+    // Constantes
+    const int gravidade = 20;
+    const int maxPulo = 3;
+    const int minOpen = 300;
+    const int JumpStrengt = 40;
+    const int fps = 50;
 
-	const int maxPulo = 3;
+    // Variáveis
+    int speed = 10;
+    int tempoPulando = 0;
+    int score = 0;
+    bool isJumping = false;
+    bool dead = true;
 
-	const int minOpen = 200;
+    double LarguraJanela = 0;
+    double AlturaJanela = 0;
 
-	const int JumpStrengt = 40;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	const int fps = 50;
+    protected override void OnSizeAllocated(double AJ, double LJ)
+    {
+        base.OnSizeAllocated(AJ, LJ);
+        LarguraJanela = LJ;
+        AlturaJanela = AJ;
 
+        if (AJ > 0)
+        {
+            CanoCima.HeightRequest = AJ - CachorroImage.HeightRequest;
+            CanoBaixo.HeightRequest = AJ - CachorroImage.HeightRequest;
+        }
+    }
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    void Inicializar()
+    {
+        FrameGameOver.IsVisible = false;
+        LabelFinalScore.IsVisible = false;
 
-	int speed = 10;
-	int tempoPulando = 0;
+        CanoBaixo.TranslationX = -LarguraJanela;
+        CanoCima.TranslationX = -LarguraJanela;
+        CachorroImage.TranslationX = 0;
+        CachorroImage.TranslationY = 0;
+        score = 0;
 
-	int score = 0;
+        AndarPipe();
+    }
 
-	bool isJumping = false;	
+    async Task Drawn()
+    {
+        while (!dead)
+        {
+            if (isJumping)
+                JumpApply();
+            else
+                CreateGravity();
 
-	bool dead = true;
-	
-	double LarguraJanela=0;
+            AndarPipe();
 
-	double AlturaJanela=0;
+            if (VerifyColisao())
+            {
+                dead = true;
+                FrameGameOver.IsVisible = true;
+                LabelFinalScore.IsVisible = true;
+                break;
+            }
 
+            await Task.Delay(fps);
+        }
+    }
 
+    void CreateGravity()
+    {
+        CachorroImage.TranslationY += gravidade;
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
+    void JumpApply()
+    {
+        CachorroImage.TranslationY -= JumpStrengt;
+        tempoPulando++;
+        if (tempoPulando > maxPulo)
+        {
+            isJumping = false;
+            tempoPulando = 0;
+        }
+    }
 
-	}
+    void AndarPipe()
+    {
+        CanoCima.TranslationX -= speed;
+        CanoBaixo.TranslationX -= speed;
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		protected override void OnSizeAllocated(double AJ, double LJ)
-		{
-			base.OnSizeAllocated(AJ, LJ);
-			LarguraJanela = LJ;
-			AlturaJanela = AJ;	//isso que faz os canos andarem
+        if (CanoBaixo.TranslationX < -LarguraJanela)
+        {
+            CanoBaixo.TranslationX = 0;
+            CanoCima.TranslationX = 0;
 
-			if(AJ > 0)
-			{
-				CanoCima.HeightRequest = AJ - CachorroImage.HeightRequest;
-				CanoBaixo.HeightRequest = AJ - CachorroImage.HeightRequest;
+            var MaxHeight = -(CanoBaixo.HeightRequest * 0.1);
+            var MinHeight = -(CanoBaixo.HeightRequest * 0.8);
 
-			}
-		}
+            CanoCima.TranslationY = Random.Shared.Next((int)MinHeight, (int)MaxHeight);
+            CanoBaixo.TranslationY = CanoCima.TranslationY + minOpen + CanoBaixo.HeightRequest;
 
-	void Inicializar()
-	{
-		FrameGameOver.IsVisible = false;
-		LabelFinalScore.IsVisible = false;
-		CanoBaixo.TranslationX = -LarguraJanela;
-		CanoCima.TranslationY = -LarguraJanela;
-		CachorroImage.TranslationX = 0;
-		CachorroImage.TranslationY = 0;
-		score = 0;
-		
-		AndarPipe();
-	}
+            score++;
+            LabelScore.Text = "Score: " + score.ToString("D5");
+            LabelFinalScore.Text = "Final Score: " + score.ToString("D5");
 
-	async Task Drawn()
-	{
-		while (!dead)
-		{
-			if(isJumping)
-			JumpApply();
-			
-			else
-			CreateGravity();
-			AndarPipe();
+            if (score % 4 == 0)
+                speed++;
+        }
+    }
 
-			if(VerifyColisao())
-			{
-				dead = true;
-				FrameGameOver.IsVisible = true;
-				LabelFinalScore.IsVisible = true;
-				break;
-			}
+    bool VerifyColisao()
+    {
+        return (!dead && (VerificaColisaoChao() || VerificaColisaoTeto() || VerificaCanoCima() || VerificaCanoBaixo()));
+    }
 
-			await Task.Delay(fps);
+    bool VerificaColisaoTeto()
+    {
+        var minY = -AlturaJanela / 2;
+        return CachorroImage.TranslationY <= minY;
+    }
 
+    bool VerificaColisaoChao()
+    {
+        var maxY = AlturaJanela / 2;
+        return CachorroImage.TranslationY >= maxY - CachorroImage.Height;
+    }
 
+    bool VerificaCanoCima()
+    {
+        var HorizontalCord = (LarguraJanela / 2) - (CachorroImage.WidthRequest / 2);
+        var VerticalCord = (AlturaJanela / 2) - (CachorroImage.HeightRequest / 2) + CachorroImage.TranslationY;
 
-		}
-	}
+        return (HorizontalCord >= Math.Abs(CanoCima.TranslationX) - CanoCima.WidthRequest &&
+                HorizontalCord <= Math.Abs(CanoCima.TranslationX) + CanoCima.WidthRequest &&
+                VerticalCord <= CanoCima.HeightRequest + CanoCima.TranslationY);
+    }
 
-	async void CreateGravity()
-	{
-		CachorroImage.TranslationY += gravidade;
-	}
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	void AndarPipe()
-	{
-		CanoCima.TranslationX -= speed;
-		CanoBaixo.TranslationX -= speed;
+    bool VerificaCanoBaixo()
+    {
+        var HorizontalCord = (LarguraJanela / 2) - (CachorroImage.WidthRequest / 2);
+        var VerticalCord = (AlturaJanela / 2) + (CachorroImage.HeightRequest / 2) + CachorroImage.TranslationY;
 
-		if (CanoCima.TranslationX < -LarguraJanela)
-		{
-			CanoCima.TranslationX = 0;
-			CanoBaixo.TranslationX = 0;
+        return (HorizontalCord >= Math.Abs(CanoCima.TranslationX) - CanoCima.WidthRequest &&
+                HorizontalCord <= Math.Abs(CanoCima.TranslationX) + CanoCima.WidthRequest &&
+                VerticalCord >= CanoCima.TranslationY + minOpen);
+    }
 
-			var MaxHeight = -(CanoBaixo.HeightRequest * 0.1);
-			var MinHeight = -(CanoBaixo.HeightRequest * 0.8);
-			
+    void Jump(object sender, TappedEventArgs args)
+    {
+        isJumping = true;
+    }
 
-			CanoCima.TranslationY = Random.Shared.Next((int)MaxHeight, (int)MinHeight);
-			CanoBaixo.TranslationY = CanoCima.TranslationY + minOpen + CanoBaixo.HeightRequest;
-
-
-
-			score++;
-			LabelScore.Text = "Canos: " + score.ToString("D3");
-
-
-			LabelFinalScore.Text = "Final Score:" + score.ToString("D3");
-
-
-		}
-
-	}
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-		bool VerifyColisao()
-		{
-
-		return (!dead && (VerificaColisaoTeto() || VerificaColisaoChao()|| VerificaCanoCima()));
-
-		}
-
-
-		bool VerificaColisaoTeto()
-		{
-			var minY = -AlturaJanela/2;
-			if (CachorroImage.TranslationY <=minY)
-			    return true;
-			else
-				return false;
-		}
-		bool VerificaColisaoChao()
-		{
-			var maxY = AlturaJanela/2;
-			if (CachorroImage.TranslationY >=maxY)
-			return true;
-			else
-			return false;
-		}
-
-
-		bool VerificaCanoCima()
-		{
-			var HorizontalCord = (LarguraJanela/2)-(CachorroImage.WidthRequest/2);
-			var VerticalCord = (AlturaJanela/2)-(CachorroImage.HeightRequest/2)+CachorroImage.TranslationY;
-
-			if (HorizontalCord >= Math.Abs(CanoCima.TranslationX)-CanoCima.WidthRequest &&
-				HorizontalCord <= Math.Abs(CanoCima.TranslationX)+CanoCima.WidthRequest &&
-				VerticalCord   <= CanoCima.HeightRequest + CanoCima.TranslationY)
-				{
-					return true;
-				}
-				else
-				{
-					return false;
-				}
-
-
-		}
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		void JumpApply()
-		{
-			CachorroImage.TranslationY -= JumpStrengt;
-			tempoPulando ++;
-			if (tempoPulando > maxPulo)
-			{
-				isJumping = false;
-				tempoPulando = 0;
-			}
-		}
-		void Jump(object sender, TappedEventArgs a)
-		{
-			isJumping = true;
-		}
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		void OnGameOverClicked(object s, TappedEventArgs a)
-
-		{	
-
-			FrameGameOver.IsVisible=false;
-			dead = false;
-			Inicializar();
-			Drawn();
-
-		}
-
+    void OnGameOverClicked(object s, TappedEventArgs args)
+    {
+        FrameGameOver.IsVisible = false;
+        dead = false;
+        Inicializar();
+        _ = Drawn();
+    }
 }
-
